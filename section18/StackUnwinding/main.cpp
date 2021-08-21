cccff4b87732087ae5f6e96167c8e996ea6cfc1d
Ank1taS <sahoosona2209@gmail.com>
2021-08-21 21:29:41 +0530

    Added finding hander by unwinding Stack.

---
+++ section18/StackUnwinding/main.cpp
@@ -0,0 +1,48 @@
+// Stack Unwinding example
+
+
+#include <iostream>
+
+void functionA();
+void functionB();
+void functionC();
+
+void functionA() {
+    std::cout << "Starting function-A" << std::endl;
+    functionB();
+    std::cout << "Ending function-A" << std::endl;
+}
+
+void functionB() {
+    std::cout << "Starting function-B" << std::endl;
+
+    try {
+        functionC();
+    }
+
+    catch (int &ex) {
+         std::cout << "Caught error in function-B" << std::endl;       
+    }
+
+    std::cout << "Ending function-B" << std::endl;
+}
+
+void functionC()  {
+    std::cout << "Starting function-C" << std::endl;
+    throw 100;
+    std::cout << "Ending function-C" << std::endl; 
+}
+
+int main() {    
+    
+    std::cout << "Starting main" << std::endl;
+    try {
+        functionA();
+    }
+    catch (int &ex) {
+        std::cout << "Caught error in main" << std::endl;   // never executed as handeled in fuctionB
+    }
+    std::cout << "Finishing main" << std::endl;
+    return 0;
+}
+
